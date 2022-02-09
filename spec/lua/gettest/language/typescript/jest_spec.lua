57039d8f5b9059776c3d016afd7e812388c137ee
notomo <notomo.motono@gmail.com>
2022-02-09 22:21:57 +0900

    Add js test

--- spec/lua/gettest/language/typescript/jest_spec.lua
+++ spec/lua/gettest/language/typescript/jest_spec.lua
@@ -1,16 +1,17 @@
 local helper = require("gettest.lib.testlib.helper")
 local gettest = helper.require("gettest")
 
-local language = "typescript"
-
 describe("with jest,", function()
   before_each(helper.before_each)
   after_each(helper.after_each)
 
-  it("works gettest.all_leaves()", function()
-    helper.install_parser(language)
+  -- HACK
+  local languages = { "typescript", "javascript" }
+  for _, language in ipairs(languages) do
+    it(language .. ", works gettest.all_leaves()", function()
+      helper.install_parser(language)
 
-    helper.set_lines([[
+      helper.set_lines([[
 describe("TestMethod1", () => {
   it("should return 11", async () => {
   });
@@ -31,33 +32,34 @@ describe("TestMethod2", () => {
 describe("TestMethod3", () => {
 });
 ]])
-    vim.bo.filetype = language
+      vim.bo.filetype = language
 
-    local tests, err = gettest.all_leaves()
-    assert.is_nil(err)
+      local tests, err = gettest.all_leaves()
+      assert.is_nil(err)
 
-    local want = {
-      {
-        name = "TestMethod1 should return 11",
-        row = helper.get_row("should return 11"),
-      },
-      {
-        name = "TestMethod1 12 should return 121",
-        row = helper.get_row("should return 121"),
-      },
-      {
-        name = "TestMethod1 12 should return 122",
-        row = helper.get_row("should return 122"),
-      },
-      {
-        name = "TestMethod2 should return 21",
-        row = helper.get_row("should return 21"),
-      },
-      {
-        name = "TestMethod3",
-        row = helper.get_row("TestMethod3"),
-      },
-    }
-    assert.test_values(tests, want)
-  end)
+      local want = {
+        {
+          name = "TestMethod1 should return 11",
+          row = helper.get_row("should return 11"),
+        },
+        {
+          name = "TestMethod1 12 should return 121",
+          row = helper.get_row("should return 121"),
+        },
+        {
+          name = "TestMethod1 12 should return 122",
+          row = helper.get_row("should return 122"),
+        },
+        {
+          name = "TestMethod2 should return 21",
+          row = helper.get_row("should return 21"),
+        },
+        {
+          name = "TestMethod3",
+          row = helper.get_row("TestMethod3"),
+        },
+      }
+      assert.test_values(tests, want)
+    end)
+  end
 end)
