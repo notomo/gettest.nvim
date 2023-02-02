c2453aeaebf1bb6c26f0ff54455fe8025cede5bd
notomo <notomo.motono@gmail.com>
2023-02-02 09:46:47 +0900

    Fix doc @return

--- lua/gettest/init.lua
+++ lua/gettest/init.lua
@@ -3,7 +3,6 @@ local M = {}
 --- Get test nodes.
 --- @param opts table|nil: |gettest.nvim-options|
 --- @return table: list of |gettest.nvim-test-node|
---- @return string|nil: error message if error
 function M.nodes(opts)
   local tests, err = require("gettest.command").nodes(opts)
   if err then
